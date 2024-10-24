0e0c8e43a321305ef8f7b9fd42ec4ebd36976ad8
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-10-24 23:21:03 +0300

    project start was made

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CVApp from "../components/CVApp"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <CVApp/>
+    </div>
+  )
+}
+
+export default App
